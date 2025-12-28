443c5d86f3ab2945e3e28cdbd3cce4abe58138cc
Vasile Bejan <124290984+vasile007@users.noreply.github.com>
2025-12-28 18:57:28 +0000

    Fix socket token redeclaration, rename to authToken

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,22 +1,22 @@
-
 import { io } from "socket.io-client";
 
 const BASE = "http://3.209.223.219:3000";
 
 export function connectSocket() {
 
-  let token = "";
+  let authToken = "";
 
   try {
-    token = localStorage.getItem("token") || "";
+    authToken = localStorage.getItem("token") || "";
   } catch {}
 
   return io(BASE, {
     transports: ["polling"],
     upgrade: false,
-    query: { token }
+    query: { token: authToken }
   });
 }
+
 
 
 
